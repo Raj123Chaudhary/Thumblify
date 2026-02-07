0989ccf4f902ebcc6914741c4c43dec32b2b73d9
Raj123Chaudhary <raj656894@gmail.com>
2026-02-07 12:49:37 +0530

    fixing bug in authController

--- backend/controllers/AuthController.ts
+++ backend/controllers/AuthController.ts
@@ -61,9 +61,9 @@ export const registerUser = async (req:Request,res:Response) =>{
 
      
    } catch (error:any) {
-      console.log("Error while creating account",error);
+      console.error("Error while creating account",error);
       res.status(500).json({
-         message:error.message
+         message:"Something went wrong , Please try again"
       })
 
 
@@ -81,11 +81,76 @@ export const loginUser = async (req:Request,res:Response) =>{
           if(!email || !password){
             return res.status(400).json({message:"All fields are required"})
           }
+    
+      // 2.find user by email
+         const user = await User.findOne({email}).select("+password");
+          if(!user){
+            return res.status(401).json({message:"Invalid email or password"})
+          }
+      // 3. checking the password
+         const isPasswordCorrect = await bcrypt.compare(password,user.password)
+         if(!isPasswordCorrect){
+            return res.status(401).json({message:"Invalid email or password"})
+         }
+      // setting user data in session
+      req.session.isLoggedIn=true;
+      req.session.userId= user._id.toString();
 
+      // return res
+      return res.status(200).json({
+         message:"Login Successfully",
+         user:{
+            _id:user._id,
+            name:user.name,
+            email:user.email
+         }
+      })
+   } catch (error:any) {
+      console.error("Login Error",error)
+      res.status(500).json({
+         message:"Something went wrong , Please try again" 
+      })
+      
+   }
+}
 
+// Controller for User logout 
+
+export const logoutUser = async (req:Request,res:Response) =>{
+   try {
+      req.session.destroy((err:any)=>{
+         if(err){
+            console.error("Logout error :",err);
+            return res.status(500).json({
+               message:"Unable to logout. please try again"
+            })
+         }
+          return res.status(200).json({
+         message:"Logout successful"
+      })
+      
+      });
+     
+   } catch (error) {
+      console.error("Logout Error : ",error);
+      return res.status(500).json({
+         message:"Something went wrong , Please try again"
+      })
+      
+   }
+}
+
+// Controller for User Verify
+
+export const verifyUser = async (req:Request,res:Response) =>{
+   try {
 
       
-   } catch (error:any) {
+   } catch (error) {
+      console.error("User Verify Error: ",error);
+      return res.status(500).json({
+         message:"Something went wrong. Please try again"
+      })
       
    }
 }
--- backend/models/user.ts
+++ backend/models/user.ts
@@ -3,14 +3,15 @@ import mongoose from "mongoose"
 export interface IUser extends Document{
      name: string;
      email:string;
-     password?:string;
+     password:string;
      createdAt?:Date;
      updatedAt?:Date;
 }
 
 const userSchema = new mongoose.Schema<IUser>({
      name :{type:String,required:true,trim:true},
-     email:{type:String,required:true,trim:true,unique:true,lowercase:true}
+     email:{type:String,required:true,trim:true,unique:true,lowercase:true},
+     password:{type:String,required:true,trim:true,select:false}
 },{timestamps:true})
 
 export default mongoose.model<IUser>("User",userSchema)
