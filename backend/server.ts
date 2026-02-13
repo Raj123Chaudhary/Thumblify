483d52f84340bcf7f1e828386f7cced8fedfec4e
Raj123Chaudhary <raj656894@gmail.com>
2026-02-13 13:49:01 +0530

    change the cokkie

--- backend/server.ts
+++ backend/server.ts
@@ -32,7 +32,7 @@ app.use(
     resave: false,
     saveUninitialized: false,
     cookie: {
-      secure: process.env.NODE_ENV === "production",
+      secure: true,
       httpOnly: true,
       sameSite: "none",
       maxAge: 1000 * 60 * 24 * 7,
